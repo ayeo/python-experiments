119f1623dbbbf5011384273359e27c1a72c37d47
ayeo <me@ayeo.pl>
2018-02-11 22:13:08 +0100

    Add better preview

--- game-of-life.py
+++ game-of-life.py
@@ -10,15 +10,24 @@ from random import randint
 
 rc('animation', html='html5')
 
-width = 100;
-height = 100;
-population = 300;
-deadZone = 5;
-edenMargin = 30;
-cmap = 'gist_heat';
+width = 250
+height = 250
+population = 5000
+deadZone = 5
+edenMargin = 50
+cmap = 'hot'
 
 fig, ax = plt.subplots()
 fig.set_tight_layout(True)
+
+def get_color():
+    if get_color.color > 100:
+        get_color.color = 30
+
+    get_color.color += 0.5
+    return get_color.color    
+        
+get_color.color = 30;
 
 def neighbors(cell):
     (x, y) = cell
@@ -31,20 +40,20 @@ def neighbor_counts(world):
                       for nb in neighbors(cell))
     
 def translate(world):
-    universe = np.zeros((width, height))
+    universe = np.full((width, height), 0)
     nnn = neighbor_counts(world)
+    #color = get_color()
     for item in world:
-        universe[item[0], item[1]] = (nnn[item] * 12)
+        universe[item[0], item[1]] = nnn[item[0], item[1]] * 12
     return universe.transpose((1, 0))    
 
 
-#world = [(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
 world = []
 
 for x in np.arange(population):
     world.append((randint(edenMargin, width - edenMargin), randint(edenMargin, height - edenMargin)))
 
-ax.imshow(translate(world), cmap=cmap)
+ax.imshow(translate(world), cmap=cmap, vmin=0, vmax=100)
 plt.axis('off')
 
 def filter(world):
@@ -64,17 +73,18 @@ def next_generation(world):
 
 def update(num):
     global world
+
     plt.cla()
     world = next_generation(world)        
     filter(world)
-    ax.imshow(translate(world), cmap=cmap)
+    ax.imshow(translate(world), cmap=cmap, vmin=0, vmax=100)
     plt.axis('off')
     return plt
 
-
+writer=animation.FFMpegWriter(bitrate=15000)
 anim = animation.FuncAnimation(fig, update, interval=200, frames=50)
-anim.save('game-of-life.mp4')
-#plt.show()
+#anim.save('game-of-life.mp4',  writer=writer)
+plt.show()
 
     
 
