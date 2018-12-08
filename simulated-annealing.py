5317beba03ecd8eb4be206c92b46453295cfef02
ayeo <me@ayeo.pl>
2018-12-08 17:09:13 +0100

    [annealing] Fix swaping cities

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -7,7 +7,7 @@ def evaluate(cities):
     distance = 0
     for index in range(len(cities)):
         a = cities[index]
-        if index == len(cities)-1:
+        if index == len(cities) - 1:
             b = cities[0]
         else:
             b = cities[index + 1]
@@ -22,14 +22,19 @@ def swap(x):
     y = np.copy(x)
     i = random.randint(0, len(x) - 1)
     j = random.randint(0, len(x) - 1)
+
+    if (i == j):
+        return swap(x)
+
     y[i], y[j] = x[j], x[i]
+
+    if (i > j):
+        y[j: i] = y[j: i][::-1]
 
     return y
 
 
-def accept_solution(solution1, solution2, temperature):
-    energy1 = evaluate(solution1)
-    energy2 = evaluate(solution2)
+def accept_solution(energy1, energy2, temperature):
     if energy1 > energy2:
         return True
     else:
@@ -43,16 +48,20 @@ def accept_solution(solution1, solution2, temperature):
 
 def process(cities_number, temperature = 800, cooling_factor = .001):
     cities = (np.random.rand(cities_number, 2) * 100).astype(int)
+    current = evaluate(cities)
+
     while temperature > 0.001:
         new_solution = swap(cities)
-        if accept_solution(cities, new_solution, temperature):
+        energy = evaluate(new_solution)
+        if accept_solution(current, energy, temperature):
             cities = new_solution
+            current = energy
 
         temperature *= 1 - cooling_factor
 
     return cities
 
-cities = process(25)
+cities = process(50, temperature = 50000)
 plt.plot(cities[:, 0], cities[:, 1], color='red', zorder=0)
 plt.scatter(cities[:, 0], cities[:, 1], marker='o')
 plt.axis('off')
